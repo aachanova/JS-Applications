0337b71e6d313d92043d018b48284da9d448476d
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2024-11-28 15:21:12 +0200

    Delete Exam-prep-18-Nov-2024 directory

--- Exam-prep-18-Nov-2024/utils/userUtils.js
+++
@@ -1,5 +0,0 @@
-export const saveUserData = (userData) => {
-    localStorage.setItem('_id', userData._id);
-    localStorage.setItem('email', userData.email);
-    localStorage.setItem('accessToken', userData.accessToken);
-};
